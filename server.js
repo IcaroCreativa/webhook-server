d085de05e99414b25b5e46787c2cc61a1c850185
IcaroCreativa <75864884+IcaroCreativa@users.noreply.github.com>
2023-12-04 00:45:42 +0100

    adding styles and users to chat

--- server.js
+++ server.js
@@ -44,78 +44,101 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Socket server Server!");
 });
 
-// Route POST pour le point d'API socket
-app.post("/api/socket", (req, res) => {
-  console.log(req.body);
-  res.status(200).send({ success: "Socket", body: req.body });
-});
-
+// Gestionnaire de connexion Socket.IO
 // Gestionnaire de connexion Socket.IO
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  // Stocker l'information sur la salle à laquelle l'utilisateur est connecté
+  let currentRoom = null;
+  let currentUser = null;
+
   // Gestionnaire de déconnexion Socket.IO
   socket.on("disconnect", () => {
     console.log("User disconnected");
+
+    // Émettre un message aux autres membres de la salle si l'utilisateur est connecté à une salle
+    if (currentRoom) {
+      console.log(
+        "message",
+        `the user ${currentUser} has been disconnected from the room ${currentRoom}`
+      );
+      io.to(currentRoom).emit(
+        "message",
+        `the user ${currentUser} has been disconnected from the room ${currentRoom}`
+      );
+    }
   });
 
-  // Gestionnaire d'événement "message" réception
+  // Gestionnaire d'événement "leave" pour quitter une salle
+  socket.on("leave", (data) => {
+    // Quitter la salle actuelle s'il y en a une
+    if (currentRoom) {
+      socket.leave(currentRoom);
+
+      // Émettre un message aux autres membres de la salle actuelle
+      io.to(currentRoom).emit(
+        "message",
+        `${currentUser} has left the room ${currentRoom}`
+      );
+      // Mettre à jour l'information sur la salle courante
+      currentRoom = null;
+
+      console.log(`User left room: ${data.roomNumber}`);
+    }
+  });
+
+  // Gestionnaire d'événement "join" pour rejoindre une salle
+  socket.on("join", (data) => {
+    // Quitter la salle actuelle
+    if (currentRoom) {
+      socket.leave(currentRoom);
+    }
+
+    // Rejoindre la salle spécifiée
+    socket.join(data.roomNumber);
+
+    // Mettre à jour l'information sur la salle courante
+    currentRoom = data.roomNumber;
+    currentUser = data.user;
+
+    console.log(`${data.user} joined room: ${data.roomNumber}`);
+    io.to(currentRoom).emit("message", {
+      message: `${currentUser} joined the room`,
+      user: "",
+    });
+  });
+
+  // Gestionnaire d'événement "message" réception dans une salle spécifique
   socket.on("message", (data) => {
     console.log(`Received message: ${data}`);
 
-    // Émettre le message à tous les clients connectés
-    io.emit("message", data);
+    // Émettre le message à tous les clients connectés dans la même salle
+    if (currentRoom) {
+      io.to(currentRoom).emit("message", { message: data, user: currentUser });
+    }
   });
 });
 
+// Gestionnaire de connexion Socket.IO
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
+
+//   // Gestionnaire de déconnexion Socket.IO
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+//   });
+
+//   // Gestionnaire d'événement "message" réception
+//   socket.on("message", (data) => {
+//     console.log(`Received message: ${data}`);
+
+//     // Émettre le message à tous les clients connectés
+//     io.emit("message", data);
+//   });
+// });
+
 // Démarrer le serveur et écouter les connexions entrantes
 server.listen(port, () => {
-  console.log(`Server running at https://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const allowedOrigin = process.env.MAIN_URL;
-// console.log(allowedOrigin);
-
-// const corsOptions = {
-//   origin: allowedOrigin,
-//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
-
-// // Create an Express app and listen for incoming requests on port 3000
-// const app = express();
-// app.use(cors(corsOptions));
-// const router = express.Router();
-// const port = process.env.PORT || 3000;
-
-// // Use middleware to parse incoming requests with JSON and URL-encoded payloads
-// app.use(express.json());
-// app.use(express.urlencoded());
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Internal Server Error");
-// });
-
-// // Handle GET requests to the root URL
-// router.get("/", (req, res) => {
-//   res.send("Welcome to the Webhook Server!");
-// });
-
-// // Handle POST requests to specific URLs i.e. webhook endpoints
-// router.post("/api/socket", (req, res) => {
-//   console.log(req.body);
-//   res.status(200).send({ succes: "Socket", body: req.body });
-// });
-
-// // Mount the router middleware
-// app.use(router);
-
-// // Start the server and listen for incoming connections
-// app.listen(port, () => {
-//   console.log(`Server running at https://localhost:${port}/`);
-// });
